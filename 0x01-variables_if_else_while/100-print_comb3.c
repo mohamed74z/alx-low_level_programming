05002d8a32968d1609e748ac933965f8118854cf
Mohamed74z <mo7amed74@gmail.com>
2023-03-18 13:32:02 -0700

    74

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int i, j;
+  for (i = 0; i <= 8; i++) {
+    for (j = i + 1; j <= 9; j++) {
+      putchar('0' + i);
+      putchar('0' + j);
+      if (i != 8 || j != 9) {
+        putchar(',');
+        putchar(' ');
+      }
+    }
+  }
+  return 0;
+}
