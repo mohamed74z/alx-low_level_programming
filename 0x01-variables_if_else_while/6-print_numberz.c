3e3622e737386b5a96d09217834a0c5387d21242
Mohamed74z <mo7amed74@gmail.com>
2023-03-18 13:14:51 -0700

    74

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,14 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	char digit = '0';
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		digit++;
+	}
+
 	putchar('\n');
 
 	return (0);
