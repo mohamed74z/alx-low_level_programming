733475db14723277eddd665fa809f66576112c18
mohamed74z <mo7amed74@gmail.com>
2023-03-22 22:28:58 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,3 @@
-/* This program prints the lowercase alphabets from a to z */
-#include <stdio.h>
-
-/* Function prototype for print_alphabet */
-void print_alphabet(void);
-
-/* Main function */
-int main(void)
-{
-	/* Call print_alphabet */
-	print_alphabet();
-
-	return 0;
-}
-
 /* Prints the lowercase alphabets from a to z */
 void print_alphabet(void)
 {
