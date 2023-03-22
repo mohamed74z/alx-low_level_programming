e349862933833fde1fad903e0b0c94c282e13b27
Mohamed74z <mo7amed74@gmail.com>
2023-03-22 09:22:54 -0700

    74

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 void print_alphabet(void) {
-    char letter = 'a';  // Start with the letter 'a'
-    
-    while (letter <= 'z') {  // Loop until the letter 'z'
-        _putchar(letter);    // Print the current letter
-        letter++;            // Increment the letter to the next one
+    char c = 'a';
+    while (c <= 'z') {
+        putchar(c++);
     }
-    
-    _putchar('\n');  // Print a newline character at the end
+    putchar('\n');
 }
