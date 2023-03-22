dd64b9ee7bde44b550b864108970c4d6ca070a48
Mohamed74z <mo7amed74@gmail.com>
2023-03-22 09:09:09 -0700

    74

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 /* This program prints "_putchar" followed by a new line */
-
 int main(void)
-	/* Description */
 {
-	/* This code prints "_putchar" followed by a new line */
 	printf("_putchar\n");
-	/* The main () function returns 0 to indicate successful program execution */
 	return (0);
 }
