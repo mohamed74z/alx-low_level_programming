8f86a8b5ec12d00d22ebc3bcbd29354000419453
mohamed74z <mo7amed74@gmail.com>
2023-03-22 23:11:09 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
-#include "stdio.h"
+#include <unistd.h> // Include the necessary header file
 
-/**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- * followed by a new line.
- *
- * Return: void.
- */
+void print_alphabet_x10(void); // Declare the function before main
+
+int main(void) {
+    print_alphabet_x10();
+    return 0;
+}
+
 void print_alphabet_x10(void) {
     int i;
     char letter;
