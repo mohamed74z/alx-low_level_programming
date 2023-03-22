bb68e43e48b76bc3bb4e93640975b64c295aae7d
Mohamed74z <mo7amed74@gmail.com>
2023-03-22 09:30:05 -0700

    74

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-void print_alphabet(void) {
-    char c = 'a';
-    while (c <= 'z') {
-        putchar(c++);
-    }
-    putchar('\n');
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z') {
+		putchar(c++);
+	}
+
+	putchar('\n');
 }
