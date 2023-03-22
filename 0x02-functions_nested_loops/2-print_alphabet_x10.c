9231a0067a9338ef8b3c8b7156582c9431529c5f
mohamed74z <mo7amed74@gmail.com>
2023-03-22 23:08:13 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,21 +6,15 @@
  *
  * Return: void.
  */
-void print_alphabet_x10(void)
-{
-  char letter;
-  int i;
-
-  i = 0;
-  while (i < 10)
-    {
-      letter = 'a';
-      while (letter <= 'z')
-        {
-          _putchar(letter);
-          letter++;
+void print_alphabet_x10(void) {
+    int i;
+    char letter;
+    for (i = 0; i < 10; i++) {
+        letter = 'a';
+        while (letter <= 'z') {
+            _putchar(letter);
+            letter++;
         }
-      _putchar('\n');
-      i++;
+        _putchar('\n');
     }
 }
