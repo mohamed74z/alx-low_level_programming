3078240115f00999d5f5c30354c5c29dcb372875
Mohamed74z <mo7amed74@gmail.com>
2023-03-23 14:31:13 -0700

    74

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,6 @@
 int _isupper(int c) {
-  // Check if the character is in the uppercase range of ASCII codes
-  if (c >= 'A' && c <= 'Z') {
-    return 1;
-  } else {
-    return 0;
-  }
+    if (c >= 'A' && c <= 'Z') {
+        return 1;   // Character is uppercase
+    }
+    return 0;       // Character is not uppercase
 }
