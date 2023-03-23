a115b86e81b4f579aac4a11e213d26fe993d254c
mohamed74z <mo7amed74@gmail.com>
2023-03-23 21:38:52 +0000

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
-int _isupper(int c) {
-    if (c >= 'A' && c <= 'Z') {
-        return 1;   // Character is uppercase
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return 1;
     }
-    return 0;       // Character is not uppercase
+    else
+    {
+        return 0;
+    }
 }
