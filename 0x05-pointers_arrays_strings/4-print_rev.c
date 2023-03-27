34050d4dba9b134397eafe8897fd18ebed179928
Mohamed74z <mo7amed74@gmail.com>
2023-03-27 15:44:46 -0700

    74

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,17 @@
  *
  * Return: void
  */
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		putchar(*str);
-		str++;
+void print_rev(char *s) {
+	// Find the length of the input string
+	int len = 0;
+	while (s[len] != '\0') {
+		len++;
 	}
+	// Print the characters of the string in reverse order
+	for (int i = len-1; i >= 0; i--) {
+		// Write each character to the console using putchar
+		putchar(s[i]);
+	}
+	// Add a new line character to the end of the output
 	putchar('\n');
 }
