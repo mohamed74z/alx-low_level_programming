b58b7c553b22ef254ce55f5ca1b9dc8310f66a80
mohamed74z <mo7amed74@gmail.com>
2023-03-27 23:07:48 +0000

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include "holberton.h"
 
-void puts_half(char *str) {
-	int len = 0;
-	while (str[len] != '\0') {
+/**
+ * puts_half - prints the second half of a string
+ * @str: the input string
+ */
+void puts_half(char *str)
+{
+	int len = 0, start, i;
+
+	while (str[len] != '\0')
 		len++;
-	}
-    
-	int start;
-	if (len % 2 == 0) {
+	
+	if (len % 2 == 0)
 		start = len / 2;
-	} else 
-	{
-		start = (len - 1) / 2;
-	}
-    
-	for (int i = start; i < len; i++)
-	{
-		putchar(str[i]);
-	}
-    
-	putchar('\n');
+	else
+		start = (len - 1) / 2 + 1;
+
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
