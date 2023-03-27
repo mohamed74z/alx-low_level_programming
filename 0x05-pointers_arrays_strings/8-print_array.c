180068557ee640e72cb52cdffc171013feb50374
mohamed74z <mo7amed74@gmail.com>
2023-03-27 23:12:43 +0000

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,11 +6,28 @@ void print_array(int *a, int n)
 
     for (i = 0; i < n; i++)
     {
-        putchar("%d", a[i]);
+        int num = a[i];
+        int div = 1;
+
+        /* get the divisor for the current number */
+        while (num / div > 9)
+            div *= 10;
+
+        /* print each digit of the number */
+        while (div > 0)
+        {
+            int digit = (num / div) % 10;
+            putchar(digit + '0');
+            div /= 10;
+        }
+
+        /* separate the numbers with a comma and space, except for the last one */
         if (i < n - 1)
         {
-            putchar(", ");
+            putchar(',');
+            putchar(' ');
         }
     }
-    putchar("\n");
+
+    putchar('\n');
 }
