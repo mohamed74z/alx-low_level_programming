3c240920548678c80ace43f7c7fd396804e6f95b
Mohamed74z <mo7amed74@gmail.com>
2023-03-27 15:58:06 -0700

    74

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ */
+void rev_string(char *s)
+{
+	int len = 0, i;
+	char temp;
+
+	/* Find the length of the input string */
+	while (s[len] != '\0') {
+		len++;
+	}
+
+	/* Swap characters from the beginning and end of the string */
+	for (i = 0; i < len / 2; i++) {
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
