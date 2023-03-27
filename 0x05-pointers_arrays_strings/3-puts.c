88b2ce0cb0c5edd84ab93e3c4c22ff052a57feca
Mohamed74z <mo7amed74@gmail.com>
2023-03-27 15:34:58 -0700

    74

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+/**
+ * _puts - prints a string to stdout, followed by a new line
+ * @str: pointer to the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
