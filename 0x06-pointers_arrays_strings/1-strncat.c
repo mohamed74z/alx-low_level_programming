bbee5e3d66f15b9e614c076de9f3fd9dc83520c3
mohamed74z <mo7amed74@gmail.com>
2023-03-30 22:16:20 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,27 @@
-#include <stdio.h>
+#include "main.h"
 
-char* _strncat(char *dest, char *src, int n) {
-    int dest_len = 0;
-    while(dest[dest_len] != '\0') {
-        dest_len++;
-    }
-    
-    for(int i = 0; i < n && src[i] != '\0'; i++) {
-        dest[dest_len + i] = src[i];
-    }
-    
-    dest[dest_len + n] = '\0';
-    
+/**
+ * _strncat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to use from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int i, j;
+
+    /* Find the end of the destination string */
+    for (i = 0; dest[i]; i++)
+        ;
+
+    /* Append the source string to the destination string, up to a maximum of n bytes */
+    for (j = 0; src[j] && j < n; j++)
+        dest[i + j] = src[j];
+
+    /* Add the null terminator to the end of the concatenated string */
+    dest[i + j] = '\0';
+
     return dest;
-}
-
-int main() {
-    char str1[50] = "Hello ";
-    char str2[] = "World!";
-    
-    printf("Before _strncat call: %s\n", str1);
-    
-    _strncat(str1, str2, 6);
-    
-    printf("After _strncat call: %s\n", str1);
-    
-    return 0;
 }
