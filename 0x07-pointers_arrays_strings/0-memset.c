8b990d5c2466fee28400fd0b59b0139a961f0327
mohamed74z <mo7amed74@gmail.com>
2023-04-03 10:01:37 +0000

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char* _memset(char* s, char b, unsigned int n)
+char* _memset(char *s, char b, unsigned int n) {
+    for (unsigned int i = 0; i < n; i++) {
+        s[i] = b;
+    }
+    return s;
+}
 
-{
-	char* p = s;
-	while (n-- > 0)
-	{
-	*p++ = b;
-	}
-	return s;
+int main() {
+    char buffer[100];
+    _memset(buffer, 'A', 50);
+    printf("%s\n", buffer);
+    return 0;
 }
