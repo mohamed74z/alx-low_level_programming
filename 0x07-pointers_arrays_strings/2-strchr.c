92955be28b1f1fcc44a08a3066096e5438218228
mohamed74z <mo7amed74@gmail.com>
2023-04-03 14:45:57 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,38 +1,12 @@
-#include <stdlib.h>
-
-char *_strchr(char *s, char c);
-
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-void _puts(char *str)
-{
-    int i = 0;
-
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-}
-
-int main(void)
-{
-    char s[] = "Hello, world!";
-    char *result = _strchr(s, 'w');
-    if (result == NULL)
-    {
-        _puts("Character not found.\n");
-    }
-    else
-    {
-        _puts("Character found at position ");
-        char pos = result - s + '0';
-        _putchar(pos);
-        _putchar('.');
-        _putchar('\n');
-    }
-    return (0);
+char *_strchr(char *s, char c) {
+	while (*s != '\0') { // iterate through the string
+		if (*s == c) { // check if the current character matches c
+			return s; // return the pointer to the matching character
+		}
+		s++; // move the pointer to the next character in the string
+	}
+	if (*s == c) { // check if the null terminator matches c
+		return s; // return the pointer to the null terminator
+	}
+	return NULL; // c not found in s
 }
