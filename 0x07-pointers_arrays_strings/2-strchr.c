bf82f69ab024860a96421572a21410c7ffdcaea6
mohamed74z <mo7amed74@gmail.com>
2023-04-03 14:46:37 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 char *_strchr(char *s, char c) {
-	while (*s != '\0') { // iterate through the string
-		if (*s == c) { // check if the current character matches c
-			return s; // return the pointer to the matching character
-		}
-		s++; // move the pointer to the next character in the string
-	}
-	if (*s == c) { // check if the null terminator matches c
-		return s; // return the pointer to the null terminator
-	}
-	return NULL; // c not found in s
+    while (*s != '\0') {
+        if (*s == c) {
+            return s; // return pointer to the first occurrence of c
+        }
+        s++; // increment the pointer to the next character in s
+    }
+    if (*s == c) {
+        return s; // handle case where c is at the end of s
+    }
+    return NULL; // handle case where c is not found in s
 }
