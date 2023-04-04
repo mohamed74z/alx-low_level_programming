bca68b87d53e533d6d5d2d309752d79bfb6b1851
Mohamed74z <mo7amed74@gmail.com>
2023-04-03 17:09:31 -0700

    74

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void print_chessboard(char (*a)[8]) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            putchar(a[i][j]);
+            putchar(' ');
+        }
+        putchar('\n');
+    }
+}
