5f052f4a70248ca2d96dbcb65a90e980b0be8d9d
mohamed74z <mo7amed74@gmail.com>
2023-04-04 00:14:27 +0000

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_chessboard - Prints the chessboard
- * @a: Pointer to the array representing the chessboard
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-void print_chessboard(char (*a)[8])
+int main(void)
 {
-    int i, j;
+    char chessboard[8][8] = {
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+    };
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            _putchar(a[i][j]);
-        }
-        _putchar('\n');
-    }
+    print_chessboard(chessboard);
+
+    return (0);
 }
+
