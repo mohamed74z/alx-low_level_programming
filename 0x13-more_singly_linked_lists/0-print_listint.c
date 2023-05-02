ac67160328aba98cb8080cf2599f92cde94fb3a2
Mohamed74z <mo7amed74@gmail.com>
2023-05-02 11:23:28 -0700

    74

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,12 +3,19 @@
 #include <stdio.h>
 #include "lists.h"
 
-size_t print_listint(const listint_t *h) {
-  size_t count = 0;
-  while (h != NULL) {
-    printf("%d\n", h->n);
-    h = h->next;
-    count++;
-  }
-  return count;
+/**
+ * main - check the code
+ * 
+ * Return: Always 0.
+ *
+ *
+ */
+	size_t print_listint(const listint_t *h) {
+	size_t count = 0;
+	while (h != NULL) {
+ 		printf("%d\n", h->n);
+ 		h = h->next;
+		count++;
+	}
+	return count;
 }
