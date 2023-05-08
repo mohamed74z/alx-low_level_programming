2035fb1c468044c9fc610169e263caf4141ef281
mohamed74z <mo7amed74@gmail.com>
2023-05-08 21:27:49 +0100

    74

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,24 +6,28 @@
  * Return: Always 0.
  */
 
-int create_file(const char *filename, char *text_content)
-{
-	int file, i = 0;
+int create_file(const char *filename, char *text_content) {
+    int fd, len, bytes_written;
+    mode_t mode = S_IRUSR | S_IWUSR;  // rw-------
 
-	if (!filename)
-		return (-1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file == -1)
-		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-	while (text_content[i])
-		i++;
-	write(file, text_content, i);
-	close(file);
-	return (1);
-}
+    if (filename == NULL) {
+        return -1;  // filename cannot be NULL
+    }
 
-	// Return success.
-	return 1;
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+    if (fd == -1) {
+        return -1;  // file cannot be created or opened
+    }
+
+    if (text_content != NULL) {
+        len = strlen(text_content);
+        bytes_written = write(fd, text_content, len);
+        if (bytes_written != len) {
+            close(fd);
+            return -1;  // write fails
+        }
+    }
+
+    close(fd);
+    return 1;  // success
 }
